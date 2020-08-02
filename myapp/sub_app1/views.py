0423cad2e05928a32c95ddb22eab2c2a6697f780
Soham Contractor <contractor.soham@gmail.com>
2020-08-02 05:32:32 +0100

    mned

--- myapp/sub_app1/views.py
+++ myapp/sub_app1/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import GetContactForm
 
 
 # Create your views here.
 
 def get_contacts(request):
-    return HttpResponse('First contact to views')
+
+    if request.method == 'POST':
+        form = GetContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+
+            print(name, email)
+    form = GetContactForm()
+    return render(request, 'form.html', {'form': form})
